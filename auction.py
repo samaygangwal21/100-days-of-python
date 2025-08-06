845dd50c1084e8aefa8b13ac872b828d53fd8bc4
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-06 20:22:51 +0530

    Improving code for Project : Auction

--- auction.py
+++ auction.py
@@ -1,10 +1,10 @@
 true_check = False
 auction = {}
 while not true_check:
-    name = input("What is your name")
+    name = input("What is your name: ")
     bid = int(input("What is your bid: $"))
     auction[name] = bid
-    bidder_check = input("Are there any other bidders? Type 'yes' or 'no'")
+    bidder_check = input("Are there any other bidders? Type 'yes' or 'no' :")
     if bidder_check != 'yes':
         true_check = True
 print(f"The winner is {max(auction, key=auction.get)} with bid of ${max(auction.values())}")
