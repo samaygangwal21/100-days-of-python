2dd056ee9833b7a139036edff2cc2e660606a98e
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-10 17:00:22 +0530

    Adding art for better output

--- Higher_or_Lower/higher_lower_game.py
+++ Higher_or_Lower/higher_lower_game.py
@@ -1,8 +1,10 @@
 from game_data import data
+from higher_lower_art import logo , vs
 import random
 current_score = 0
 choice_a = random.choice(data)
 choice_b = random.choice(data)
+print(logo)
 def choice(user_choice):
     if user_choice in ('A' , 'B'):
         if user_choice == 'A' :
@@ -13,19 +15,21 @@ def choice(user_choice):
         return False
 while True :
     if choice_a == choice_b :
-    choice_b = random.choice(data)
+        choice_b = random.choice(data)
     print(f"Compare A : {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}")
-    print("VS")
+    print(vs)
     print(f"Compare B : {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}")
     user_input = input("Who has more followers? Type 'A' or 'B': ").upper()
     if choice(user_input) == True:
         current_score += 1
         print("\n" * 20)
+        print(logo)
         print(f"You are right! Current score: {current_score}")
         choice_a = choice_b
         choice_b = random.choice(data)
     else :
         print("\n" *20)
+        print(logo)
         print(f"Sorry, that's wrong. Final score: {current_score}")
         break
 
