627125472d081c38ba68808bea92e97c5edf1a7d
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-17 10:39:35 +0530

    Introducing Food and regenerate it as snake eats it

--- Snake game/snake_game.py
+++ Snake game/snake_game.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
@@ -9,6 +10,7 @@ screen.title("My snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +23,11 @@ game_is_on = True
 while game_is_on :
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 
 
