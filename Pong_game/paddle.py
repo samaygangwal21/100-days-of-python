6c1a181291a0096222d5302732039c119826d3bf
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-18 17:20:22 +0530

    Pong Game is ready to be played....

--- Pong_game/paddle.py
+++ Pong_game/paddle.py
@@ -5,7 +5,6 @@ class Paddle(Turtle):
     def __init__(self, position):
         super().__init__()
         self.shape("square")
-        self.speed(0)
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
--- Pong_game/pong_game.py
+++ Pong_game/pong_game.py
@@ -1,7 +1,9 @@
 from turtle import Screen, Turtle
+
 from paddle import Paddle
 from ball import Ball
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -22,7 +25,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,11 +35,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    if ball.xcor() > 370 :
+    if ball.xcor() > 380 :
         ball.reset_position()
+        scoreboard.l_point()
 
-    if ball.xcor() < -370 :
+    if ball.xcor() < -380 :
         ball.reset_position()
+        scoreboard.r_point()
 
 
 
