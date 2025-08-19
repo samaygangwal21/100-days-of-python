62a006b2fa7d548ad9eae3dd5eed2ec0caa1e66e
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-19 15:38:59 +0530

    The Game is ready to be played.....

--- Turtle_Crossing_game/main.py
+++ Turtle_Crossing_game/main.py
@@ -4,34 +4,41 @@ from car_manager import CarManager
 from player import Player
 from scoreboard import Scoreboard
 
-
-
+# Screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.title("Turtle Crossing Game")
 
+# Game objects
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
+# Controls
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
-
+# Game loop
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # Create and move cars
     car_manager.create_car()
     car_manager.move_cars()
 
+    # Detect collision with cars
     for car in car_manager.all_cars:
-        if car.distance(player) < 20:
+        if car.distance(player) < 25:
             game_is_on = False
+            scoreboard.game_over()
 
-    if player.ycor() > 280:
+    # Detect finish line
+    if player.ycor() > 280 :
         player.restart()
         car_manager.level_up()
-
-
+        scoreboard.increase_level()
 
 screen.exitonclick()
