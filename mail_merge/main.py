74e95ecef6aa282fc491f77c0f66dc14eb61613f
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-20 17:46:18 +0530

    Learning to create new file

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -2,5 +2,8 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("my_file.txt", mode="a") as file:
-    file.write("\nNew text")
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew text")
+
+with open("new_file.txt", mode="w") as file:
+    file.write("New text")
