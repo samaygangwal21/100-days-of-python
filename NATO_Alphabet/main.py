177d8c5bdb9130c1998ac886132a593b1456782b
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-22 16:03:49 +0530

    Learning DIctionary Comprehension.

--- NATO_Alphabet/main.py
+++ NATO_Alphabet/main.py
@@ -1,3 +1,4 @@
+import random
 numbers = [1, 2, 3]
 new_list = [n*n for n in numbers if n < 3]
 print(new_list)
@@ -10,3 +11,7 @@ short_names = [name for name in names if len(name) < 5 ]
 print(short_names)
 capital_names = [name.upper() for name in names if len(name) > 5]
 print(capital_names)
+
+# Dictionary Comprehension
+students_score = {student:random.randint(60,100) for student in names}
+print(students_score)
