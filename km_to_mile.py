57384e9c23a6f437257fff40429edfa06137f613
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-23 17:38:13 +0530

    .grid() function in Tkinter

--- km_to_mile.py
+++ km_to_mile.py
@@ -7,7 +7,7 @@ window.minsize(width=500, height=300)
 #LABEL
 
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.grid(row=0, column=0)
 
 #Button
 
@@ -16,13 +16,13 @@ def button_clicked():
     my_label.config(text=input.get(), font=("Arial", 24, "bold"))
 
 button = Button(text = "Click Me", command = button_clicked)
-button.pack()
+button.grid(row=1, column=1)
 
 
 #Entry
 
 input = Entry(width=10)
-input.pack()
+input.grid(row=2, column=2)
 print(input.get())
 
 
