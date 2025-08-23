4adf7750bcd5a4e2d40cf332888ff1856da1616e
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-23 16:27:47 +0530

    Learning about *args

--- km_to_mile.py
+++ km_to_mile.py
@@ -1,17 +1,20 @@
-import tkinter
+# import tkinter
+#
+# window = tkinter.Tk()
+# window.title("MY First GUI Program")
+# window.minsize(width=500, height=300)
+#
+# #LABEL
+#
+# my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+# my_label.pack(side="left")
+#
+# window.mainloop()
 
-window = tkinter.Tk()
-window.title("MY First GUI Program")
-window.minsize(width=500, height=300)
+def add(*args):
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
 
-#LABEL
-
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-
-
-
-
-
-window.mainloop()
+print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
