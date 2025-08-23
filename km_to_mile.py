e6b29d30789c5477b33ae2a64e1bd05169107d82
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-23 16:34:06 +0530

    Learning about **kwargs

--- km_to_mile.py
+++ km_to_mile.py
@@ -11,10 +11,22 @@
 #
 # window.mainloop()
 
-def add(*args):
-    sum = 0
-    for n in args:
-        sum += n
-    return sum
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum += n
+#     return sum
+# print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
-print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+def calculate(n,**kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2,add=3, multiply=5)
+
