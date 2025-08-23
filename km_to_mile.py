7a870719da7639bfaeaff9292b83a07950f16183
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-23 16:52:35 +0530

    Learning about Entry in Tkinter

--- km_to_mile.py
+++ km_to_mile.py
@@ -9,12 +9,21 @@ window.minsize(width=500, height=300)
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+#Button
+
 def button_clicked():
     global my_label
-    my_label.config(text="I got clicked", font=("Arial", 24, "bold"))
+    my_label.config(text=input.get(), font=("Arial", 24, "bold"))
 
 button = Button(text = "Click Me", command = button_clicked)
 button.pack()
+
+
+#Entry
+
+input = Entry(width=10)
+input.pack()
+print(input.get())
 
 
 
