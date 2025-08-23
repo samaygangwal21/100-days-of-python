3b3e3b8b3611f39deafc04a9c1808bb6540caebf
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-23 16:49:14 +0530

    Learning Button in Tkinter

--- km_to_mile.py
+++ km_to_mile.py
@@ -1,32 +1,28 @@
-# import tkinter
-#
-# window = tkinter.Tk()
-# window.title("MY First GUI Program")
-# window.minsize(width=500, height=300)
-#
-# #LABEL
-#
-# my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-# my_label.pack(side="left")
-#
-# window.mainloop()
+from tkinter import *
 
-# def add(*args):
-#     sum = 0
-#     for n in args:
-#         sum += n
-#     return sum
-# print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+window = Tk()
+window.title("MY First GUI Program")
+window.minsize(width=500, height=300)
 
-def calculate(n,**kwargs):
-    print(kwargs)
-    # for key, value in kwargs.items():
-    #     print(key)
-    #     print(value)
-    n += kwargs["add"]
-    n *= kwargs["multiply"]
-    print(n)
+#LABEL
+
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+def button_clicked():
+    global my_label
+    my_label.config(text="I got clicked", font=("Arial", 24, "bold"))
+
+button = Button(text = "Click Me", command = button_clicked)
+button.pack()
 
 
-calculate(2,add=3, multiply=5)
+
+
+
+
+
+window.mainloop()
+
+
 
