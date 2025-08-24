38a4cf1571ccf49d0371aa5b69b1cf3a8698a11a
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-24 18:22:12 +0530

    Completing Pomodoro GUI Application

--- pomodoro_timer/main.py
+++ pomodoro_timer/main.py
@@ -1,23 +1,57 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 25 * 60
+SHORT_BREAK_MIN = 5 * 60
+LONG_BREAK_MIN = 20 * 60
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    tick_label.config(text="")
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps +=1
+    if reps % 8 == 0:
+        timer_label.config(text = "Long Break", fg = RED)
+        count_down(LONG_BREAK_MIN)
+        tick_label.config(text="✓")
+        reps = 0
+    elif reps % 2 == 0:
+        timer_label.config(text="Short Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN)
+    else:
+        timer_label.config(text="Work Time")
+        count_down(WORK_MIN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global  timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark += "✓"
+        tick_label.config(text= mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -29,18 +63,17 @@ tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
-count_down(5)
 
 timer_label = Label(text="Timer", fg = GREEN, bg = YELLOW, font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text = "Start", highlightthickness=0)
+start_button = Button(text = "Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text = "Reset", highlightthickness=0)
+reset_button = Button(text = "Reset", highlightthickness=0, command= reset_timer)
 reset_button.grid(column=2, row=2)
 
-tick_label = Label(text = "✓", fg = GREEN, bg = YELLOW, font=(FONT_NAME, 50))
+tick_label = Label(text = "", fg = GREEN, bg = YELLOW)
 tick_label.grid(column=1, row=3)
 
 
