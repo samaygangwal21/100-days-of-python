f5b2f6879a3473a5aa2805b31b5379936fafa426
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-24 15:52:38 +0530

    Completing User Interface

--- pomodoro_timer/main.py
+++ pomodoro_timer/main.py
@@ -23,7 +23,24 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", fg = GREEN, bg = YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text = "Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text = "Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+tick_label = Label(text = "✓", fg = GREEN, bg = YELLOW, font=(FONT_NAME, 50))
+tick_label.grid(column=1, row=3)
+
+
+
+
+
 
 
 window.mainloop()
