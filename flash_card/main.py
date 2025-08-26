c2fe0a8fd226389eebeef88b84cc9f395dcbd0c1
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-26 15:11:02 +0530

    Removing the known words

--- flash_card/main.py
+++ flash_card/main.py
@@ -3,9 +3,13 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-current_card = {}
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    current_card = {}
 
 def next_card():
     global current_card, flip_timer
@@ -21,6 +25,11 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image = card_back_img)
 
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 window = Tk()
 window.title("Flashy")
@@ -42,7 +51,7 @@ unknown_button = Button(image=cross_image, highlightthickness = 0, command=next_
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness = 0)
+known_button = Button(image=check_image, highlightthickness = 0, command = is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
