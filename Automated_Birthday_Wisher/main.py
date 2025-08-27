6b69aa7d30cb37a5b6a2385aab9a913d1ed3a72b
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-27 15:45:41 +0530

    Learning about datetime module

--- Automated_Birthday_Wisher/main.py
+++ Automated_Birthday_Wisher/main.py
@@ -1,13 +1,25 @@
-import smtplib
+# import smtplib
+#
+# my_email = "samaygangwal07@gmail.com"
+# password = "ejmcksxmyxqljunz"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="samaygangwal21@gmail.com",
+#         msg="Subject:HELLO!!\n\nThis is the body of email"
+#     )
 
-my_email = "samaygangwal07@gmail.com"
-password = "ejmcksxmyxqljunz"
+import datetime as dt
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="samaygangwal21@gmail.com",
-        msg="Subject:HELLO!!\n\nThis is the body of email"
-    )
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+date_of_birth = dt.datetime(year= 2004, month= 7, day=21)
+print(now)
+print(date_of_birth)
+print(year, month, day_of_week)
+print(now - date_of_birth)
