bfc8cad5d2fab859fb1c85805b3e7d401e8f3f3f
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-27 17:07:26 +0530

    Birthday Wisher using python

--- Automated_Birthday_Wisher/main.py
+++ Automated_Birthday_Wisher/main.py
@@ -1,25 +1,29 @@
-# import smtplib
-#
-# my_email = "samaygangwal07@gmail.com"
-# password = "ejmcksxmyxqljunz"
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="samaygangwal21@gmail.com",
-#         msg="Subject:HELLO!!\n\nThis is the body of email"
-#     )
+from datetime import datetime
+import smtplib
+import pandas
+import random
 
-import datetime as dt
+MY_EMAIL = "samaygangwal07@gmail.com"
+MY_PASSWORD = "################"
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-date_of_birth = dt.datetime(year= 2004, month= 7, day=21)
-print(now)
-print(date_of_birth)
-print(year, month, day_of_week)
-print(now - date_of_birth)
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
+
