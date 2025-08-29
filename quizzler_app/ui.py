167f84c4eebb46d18634fe73817c9d2f8fc3ace3
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-29 16:54:31 +0530

    Blinking Green/Red for Correct/Wrong Answer

--- quizzler_app/ui.py
+++ quizzler_app/ui.py
@@ -26,11 +26,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan = 2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command = self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command = self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -39,5 +39,21 @@ class QuizInterface:
 
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
