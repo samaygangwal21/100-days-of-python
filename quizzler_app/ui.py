afb56ec4cc69b20e7dea104af9ac6a12c239e815
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-08-29 17:13:01 +0530

    Completing the Quizzler

--- quizzler_app/ui.py
+++ quizzler_app/ui.py
@@ -40,18 +40,24 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You have reached the end of the Quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
 
     def true_pressed(self):
-        is_right = self.quiz.check_answer("True")
-        self.give_feedback(is_right)
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def false_pressed(self):
         is_right = self.quiz.check_answer("False")
         self.give_feedback(is_right)
 
-    def give_feedback(self):
+    def give_feedback(self, is_right):
         if is_right:
             self.canvas.config(bg="green")
         else:
