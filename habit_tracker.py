7d32c68850dddb56e4e79e8512b553e25f3f0a1f
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-09-02 10:57:32 +0530

    Creating a Graph on Pixela

--- habit_tracker.py
+++ habit_tracker.py
@@ -1,15 +1,32 @@
 import requests
 
 USERNAME = "samay"
+TOKEN = "sflfl93y498hrc3qu4i38"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "sflfl93y498hrc3qu4i38",
+    "token": TOKEN,
     "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "cycgraph",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "momiji"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
