db5f1be825508fd0c518c53368337763607f490e
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-09-02 16:56:26 +0530

    Completing the Habit Tracker

--- habit_tracker.py
+++ habit_tracker.py
@@ -1,57 +1,85 @@
 import requests
 from datetime import datetime
+import os
 
-USERNAME = "samay"
-TOKEN = "sflfl93y498hrc3qu4i38"
-GRAPH_ID = "cycgraph"
-pixela_endpoint = "https://pixe.la/v1/users"
+# Your personal data. Used by Nutritionix to calculate calories.
+GENDER = "male"
+WEIGHT_KG = 84
+HEIGHT_CM = 180
+AGE = 32
 
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
+# Nutritionix APP ID and API Key. Actual values are stored as environment variables.
+APP_ID = os.environ["ENV_NIX_APP_ID"]
+API_KEY = os.environ["ENV_NIX_API_KEY"]
 
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
-graph_config = {
-    "id": "cycgraph",
-    "name": "Cycling Graph",
-    "unit": "Km",
-    "type": "float",
-    "color": "momiji"
-}
+exercise_text = input("Tell me which exercises you did: ")
+
+# Nutritionix API Call
 headers = {
-    "X-USER-TOKEN": TOKEN
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
 }
 
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
-
-pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
-
-today = datetime.now()
-
-pixel_data = {
-    "date": today.strftime("%Y%m%d"),
-    "quantity": "4.74",
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
 }
 
-# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
-# print(response.text)
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(f"Nutritionix API call: \n {result} \n")
 
-update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
-new_pixel_daata = {
-    "quantity": "4.5"
-}
-# response = requests.put(url=update_endpoint, json=new_pixel_daata, headers=headers)
-# print(response.text)
+# Adding date and time
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
 
-delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# Sheety Project API. Check your Google sheet name and Sheety endpoint
+GOOGLE_SHEET_NAME = "workout"
+sheet_endpoint = os.environ[
+    "ENV_SHEETY_ENDPOINT"]
 
-response = requests.delete(url=delete_endpoint, headers=headers)
-print(response.text)
+# Sheety API Call & Authentication
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        GOOGLE_SHEET_NAME: {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    # Sheety Authentication Option 1: No Auth
+    """
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+    """
+
+    # Sheety Authentication Option 2: Basic Auth
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        auth=(
+            os.environ["ENV_SHEETY_USERNAME"],
+            os.environ["ENV_SHEETY_PASSWORD"],
+        )
+    )
+
+    # Sheety Authentication Option 3: Bearer Token
+    """
+    bearer_headers = {
+        "Authorization": f"Bearer {os.environ['ENV_SHEETY_TOKEN']}"
+    }
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        headers=bearer_headers
+    )    
+    """
+    print(f"Sheety Response: \n {sheet_response.text}")
