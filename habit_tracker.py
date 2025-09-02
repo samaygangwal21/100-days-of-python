86cdb092b0c6c36575ce5c727db2a4377e01348c
SAMAY GANGWAL <samaygangwal21@gmail.com>
2025-09-02 11:04:18 +0530

    Updating a pixel in pixela

--- habit_tracker.py
+++ habit_tracker.py
@@ -2,7 +2,7 @@ import requests
 
 USERNAME = "samay"
 TOKEN = "sflfl93y498hrc3qu4i38"
-
+GRAPH_ID = "cycgraph"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -28,5 +28,15 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20250902",
+    "quantity": "9.74",
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
